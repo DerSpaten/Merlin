ee0af820a213c3aea6e032bc8cce68d6e25c8650
DerSpaten <grg.play.games@gmail.com>
2018-03-14 19:55:26 +0100

    First MUI upload

---
+++ MUI/MComboBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace TestMod.MUI
+{
+    /**
+    <summary>   A combo box. (incomplete) </summary>
+    
+    <seealso cref="T:TestMod.MUI.MComponent"/>
+    **/
+
+    public class MComboBox : MComponent
+    {
+        /**
+        <summary>   Default constructor. </summary>
+        
+        <param name="prefab">   (Optional) The prefab. </param>
+        **/
+
+        public MComboBox(GameObject prefab = null)
+        {
+            try
+            {
+                if (prefab == null)
+                {
+                    gameobject = GameObject.Instantiate(GameAccess.Prefabs.ComboBox1);
+                }
+                else
+                {
+                    gameobject = GameObject.Instantiate(prefab);
+                }
+                gameobject.name = "MComboBox";
+                RectTransform = gameobject.GetComponentInChildren<RectTransform>();
+            }catch(Exception e)
+            {
+                Debug.Log(e);
+            }
+        }                      
+    }
+}
